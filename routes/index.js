6a56c0180ac3c929993aee6b1966116ce50d117c
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-10-30 03:38:16 +0200

    change routes

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var LCtrl = require('../controllers/leads')
+var Ctrl = require('../controllers/leads')
 const fse = require('fs-extra')
 const path = require('path')
 
-var config = fse.readJSONSync(path.join(__dirname, '..', 'config.json'));
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: config.appName });
-});
+router.get('/', Ctrl.index);
 
-router.get('/leads', LCtrl.getLeads);
+router.get('/add', Ctrl.getLeads);
 
-router.post('/leads/add', LCtrl.addLead);
+router.post('/settings', Ctrl.addLead);
 
-router.get('/leads/delete/:id', LCtrl.deleteLead);
+router.get('/delete/:id', Ctrl.deleteLead);
 
-router.get('/leads/updateDate/:id', LCtrl.updateLeadDate);
-
-router.get("/leads/:id", LCtrl.geSingleLead)
 
 module.exports = router;
